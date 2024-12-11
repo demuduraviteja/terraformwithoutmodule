bf42f4e4f8667677cb73ee19b9ef44e46698f650
raviteja <raviteja.10711210@ltimindtree.com>
2024-12-11 22:54:14 +0530

    added terraform without module

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+ami_id        = "ami-0327f51db613d7bd2"
+aws_region    = "ap-south-1"
+instance_type = "t2.micro"
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  type = string
+}
+variable "ami_id" {
+  type = string
+}
+variable "instance_type" {
+  type = string
+}
